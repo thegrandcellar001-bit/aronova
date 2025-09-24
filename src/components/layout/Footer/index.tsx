aaacc17902e3f5c6d5d03e251779433a2de0154f
Dev Chris <hi.chrisdev@gmail.com>
2025-09-24 18:49:36 +0100

    Made some few changes

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -74,7 +74,7 @@ const Footer = () => {
                   "text-[28px] lg:text-[32px] mb-6",
                 ])}
               >
-                SHOP.CO
+                Aronova
               </h1>
               <p className="text-black/60 text-sm mb-9">
                 We have clothes that suits your style and which you’re proud to
@@ -103,20 +103,9 @@ const Footer = () => {
           <hr className="h-[1px] border-t-black/10 mb-6" />
           <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center mb-2">
             <p className="text-sm text-center sm:text-left text-black/60 mb-4 sm:mb-0 sm:mr-1">
-              Shop.co © Made by{" "}
-              <Link
-                href="https://github.com/mohammadoftadeh"
-                className="text-black font-medium"
-              >
-                Mohammad Oftadeh
-              </Link>
-              {", "}
-              Designed by{" "}
-              <Link
-                href="https://www.figma.com/@hamzauix"
-                className="text-black font-medium"
-              >
-                Hamza Naeem
+              © {new Date().getFullYear()}{" "}
+              <Link href="#" className="text-black font-medium">
+                Aronova
               </Link>
             </p>
             <div className="flex items-center">
--- src/components/layout/Navbar/TopNavbar/index.tsx
+++ src/components/layout/Navbar/TopNavbar/index.tsx
@@ -84,7 +84,7 @@ const TopNavbar = () => {
               "text-2xl lg:text-[32px] mb-2 mr-3 lg:mr-10",
             ])}
           >
-            SHOP.CO
+            Aronova
           </Link>
         </div>
         <NavigationMenu className="hidden md:flex mr-2 lg:mr-7">
