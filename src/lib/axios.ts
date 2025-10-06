0d264ba6afb391c81050f71046e8510cf4853f59
Dev Chris <hi.chrisdev@gmail.com>
2025-10-06 14:07:14 +0100

    Fixed: redirection issues from axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 import { store } from "./store";
-import { setToken, logout } from "./features/auth/authSlice";
+import { logout } from "./features/auth/authSlice";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -24,33 +24,10 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (response) => response,
   async (error) => {
-    const originalRequest = error.config;
-
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-
-      try {
-        const res = await axios.post(
-          "/api/auth/refresh",
-          {},
-          { withCredentials: true }
-        );
-
-        if (res.status === 200) {
-          const { token } = res.data;
-
-          store.dispatch(setToken(token));
-
-          originalRequest.headers.Authorization = `Bearer ${token}`;
-          return api(originalRequest);
-        }
-      } catch (refreshErr) {
-        store.dispatch(logout());
-        return Promise.reject(refreshErr);
-      }
+    if (error.response?.status === 401) {
+      store.dispatch(logout());
+      return Promise.reject(error);
     }
-
-    return Promise.reject(error);
   }
 );
 
