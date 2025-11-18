4a94c96f54b90733108d5dc543d2050a2f4e941f
Dev Chris <hi.chrisdev@gmail.com>
2025-11-18 19:25:01 +0100

    Revamped: the footer

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,38 +1,10 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import { useToast } from "@/hooks/use-toast";
-import axios from "axios";
 import { Mail, MapPin, Phone } from "lucide-react";
 import Link from "next/link";
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 
 export default function TermsPage() {
-  const [sending, setSending] = useState<boolean>(false);
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
-  });
-
-  const { toastSuccess, toastError } = useToast();
-
-  const handleSubmission = async () => {
-    setSending(true);
-
-    try {
-      await axios.post("/customer/support", { formData });
-      toastSuccess("Message sent.");
-    } catch (e) {
-      toastError("An error occurred when sending the message.");
-    } finally {
-      setSending(false);
-    }
-  };
-
   return (
     <main className="max-w-frame mx-auto px-4 xl:px-0 pt-28 pb-10">
       <h1 className="text-3xl font-bold mb-4">Your Curator, Always On Hand.</h1>
@@ -54,90 +26,6 @@ export default function TermsPage() {
         return it within 14 days. Our concierge team will arrange collection and
         ensure the process is seamless.
       </p>
-
-      <div className="max-w-6xl mt-10">
-        <h3 className="font-semibold text-2xl mb-1">Reach Out</h3>
-        <p className="text-black/70">
-          You can reach out to us through the form below.
-        </p>
-        <div className="flex flex-col gap-y-4 mt-6">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <Input
-              type="text"
-              className="h-12"
-              placeholder="Name"
-              onChange={(e) =>
-                setFormData({
-                  ...formData,
-                  name: e.target.value,
-                })
-              }
-              value={formData.name}
-              required
-            />
-            <Input
-              type="email"
-              className="h-12"
-              placeholder="E-mail address"
-              onChange={(e) =>
-                setFormData({
-                  ...formData,
-                  email: e.target.value,
-                })
-              }
-              value={formData.email}
-              required
-            />
-          </div>
-          <div>
-            <Input
-              type="text"
-              className="h-12"
-              placeholder="Subject"
-              onChange={(e) =>
-                setFormData({
-                  ...formData,
-                  subject: e.target.value,
-                })
-              }
-              value={formData.subject}
-              required
-            />
-          </div>
-          <div>
-            <Textarea
-              className="h-32"
-              placeholder="Your message"
-              onChange={(e) =>
-                setFormData({
-                  ...formData,
-                  message: e.target.value,
-                })
-              }
-              value={formData.message}
-              required
-            ></Textarea>
-          </div>
-          <Button
-            variant="default"
-            className="transition cursor-pointer w-fit"
-            disabled={
-              !formData.email ||
-              !formData.message ||
-              !formData.name ||
-              !formData.subject
-            }
-          >
-            {sending ? (
-              "Sending..."
-            ) : (
-              <Fragment>
-                Send Message <i className="fas fa-send" />
-              </Fragment>
-            )}
-          </Button>
-        </div>
-      </div>
 
       <div className="py-20 px-6">
         <div className="max-w-6xl mx-auto">
